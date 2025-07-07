b73ee325e6a59ffa0f2c0b22fd4729759d650530
albayrrak <albayrrak@gmail.com>
2025-07-07 13:29:58 +0300

    db reset

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
 import { Button } from '@/components/ui/button';
-import { prisma } from '@/lib/prisma';
 import React from 'react';
 
-const Page = async () => {
-  const users = await prisma.user.findMany();
-
+const Page = () => {
   return <div className="font-bold text-rose-500"></div>;
 };
 
