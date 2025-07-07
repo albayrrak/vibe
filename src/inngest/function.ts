6f9517737dceca53666c6961d8332727de918ee7
yücel albayrak <albayrrak@gmail.com>
2025-07-07 22:31:15 +0300

    open ai api

--- src/inngest/function.ts
+++ src/inngest/function.ts
@@ -1,12 +1,20 @@
+import { Agent, openai, createAgent } from "@inngest/agent-kit";
+
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
     { id: "hello-world" },
     { event: "test/hello.world" },
     async ({ event, step }) => {
-        await step.sleep("wait-a-moment", "30s");
-        await step.sleep("wait-a-moment", "10s");
-        await step.sleep("wait-a-moment", "5s");
-        return { message: `Hello ${event.data.email}!` };
+        const codeAgent = createAgent({
+            name: "code-agent",
+            system: "You are an expert next.js developer. You write readable, maintainable code. You write simple Next.js & React snippets.",
+            model: openai({ model: "gpt-4o", apiKey: process.env.OPENAI_API_KEY })
+        })
+
+        const { output } = await codeAgent.run(`Write the following snippet: ${event.data.value}`);
+
+
+        return { output };
     },
 );
